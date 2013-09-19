2f3bf4f9c6835568c0ebb65eaee7a0c881c685cd
Kris Jenkins <krisajenkins@gmail.com>
2013-09-19 12:43:29 +0100

    WIP: We've got something like a working function constructor.
    Handles docstrings, doesn't handle arguments.

--- src/sqlinsql/core.clj
+++ src/sqlinsql/core.clj
@@ -11,14 +11,18 @@
 (defn extract-docstring
   "Returns the docstring, if any, within the given sqlfile."
   [sqlfile]
-  ""
-
-  )
+  (->> sqlfile
+       (re-seq #"(?m)^\s*--\s*(.*)")
+       (map second)
+       (join "\n")))
 
 (defn extract-query
   "Returns the query for the given sqlfile."
   [sqlfile]
-  sqlfile)
+  (->> sqlfile
+       (re-seq #"(?m)^(?!\s*--\s*)(.*)")
+       (map second)
+       (join "\n")))
 
 (defn extract-parameters
   "Returns a sequence of the parameter names for the query"
@@ -26,8 +30,35 @@
   )
 
 (defn make-query-function
-  [sql]
+  [query]
   (fn [db & parameters]
     (lazy-seq
      (sql/query db
-                (into [] (cons sql parameters))))))
+                (cons query parameters)))))
+
+(defmacro defquery
+  "Defines a query function, as defined in the given SQL file.
+Any comments in that file will form the docstring."
+  [name file]
+  (let [sqlfile (slurp-from-classpath file)
+        query (extract-query sqlfile)
+        docstring (extract-docstring sqlfile)
+        ]
+    `(def ~(with-meta name
+             (merge (meta name)
+                    {:arglists ''([db & parameters])
+                     :doc docstring}))
+       (fn [db# & parameters#]
+         (lazy-seq
+          (sql/query db#
+                     (cons ~query parameters#)))))))
+
+(defquery current-time "sqlinsql/current_time.sql")
+
+(def db {:subprotocol "derby"
+         :subname "sqlinsql_test_derby"
+         :create true})
+
+(:time
+ (first
+  (current-time db)))
--- test/sqlinsql/core_test.clj
+++ test/sqlinsql/core_test.clj
@@ -10,7 +10,8 @@
 (deftest startup-test-db
   (sql/with-connection derby-db
     (sql/with-query-results rows ["SELECT CURRENT_TIMESTAMP FROM SYSIBM.SYSDUMMY1"]
-      (is (= 1 (count rows))))))
+      (is (= (count rows)
+             1)))))
 
 (deftest slurp-from-classpath-test
   (is (re-find #"SELECT"
@@ -18,6 +19,12 @@
 
 (deftest extraction
   (let [current-time-file (slurp-from-classpath "sqlinsql/current_time.sql")]
+    (testing "Docstring extraction."
+      (is (= (extract-docstring current-time-file)
+             "Just selects the current time.\nNothing fancy.")))
+    (testing "Query extraction."
+      (is (= (extract-query current-time-file)
+             "SELECT CURRENT_TIMESTAMP AS time\nFROM SYSIBM.SYSDUMMY1")))
     (testing "Query function - select current time."
       (let [current-time-fn (make-query-function "SELECT CURRENT_TIMESTAMP AS time\nFROM SYSIBM.SYSDUMMY1")
             [{current-time :time}] (current-time-fn derby-db)]
