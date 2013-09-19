63fe05da68c431bb45f139a2a8f91433e09f51d2
Kris Jenkins <krisajenkins@gmail.com>
2013-09-19 11:22:18 +0100

    Adding a query-to-function first-pass implementation.

--- src/sqlinsql/core.clj
+++ src/sqlinsql/core.clj
@@ -1,6 +1,33 @@
-(ns sqlinsql.core)
+(ns sqlinsql.core
+  (:require [clojure.java.io :refer [resource]]
+            [clojure.java.jdbc :as sql]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn slurp-from-classpath
+  "Slurps a file from the classpath."
+  [path]
+  (if-let [file (resource path)]
+    (slurp file)))
+
+(defn extract-docstring
+  "Returns the docstring, if any, within the given sqlfile."
+  [sqlfile]
+  ""
+
+  )
+
+(defn extract-query
+  "Returns the query for the given sqlfile."
+  [sqlfile]
+  sqlfile)
+
+(defn extract-parameters
+  "Returns a sequence of the parameter names for the query"
+  [query]
+  )
+
+(defn make-query-function
+  [sql]
+  (fn [db & parameters]
+    (lazy-seq
+     (sql/query db
+                (into [] (cons sql parameters))))))
--- test/sqlinsql/core_test.clj
+++ test/sqlinsql/core_test.clj
@@ -12,3 +12,15 @@
     (sql/with-query-results rows ["SELECT CURRENT_TIMESTAMP FROM SYSIBM.SYSDUMMY1"]
       (is (= 1 (count rows))))))
 
+(deftest slurp-from-classpath-test
+  (is (re-find #"SELECT"
+               (slurp-from-classpath "sqlinsql/current_time.sql"))))
+
+(deftest extraction
+  (let [current-time-file (slurp-from-classpath "sqlinsql/current_time.sql")]
+    (testing "Query function - select current time."
+      (let [current-time-fn (make-query-function "SELECT CURRENT_TIMESTAMP AS time\nFROM SYSIBM.SYSDUMMY1")
+            [{current-time :time}] (current-time-fn derby-db)]
+        (is (instance? java.util.Date current-time)))
+      )
+    )) 
