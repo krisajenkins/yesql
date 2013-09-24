3ce2370a0304507bb3ceac7dd42c164efe930b99
Kris Jenkins <krisajenkins@gmail.com>
2013-09-24 11:29:55 +0100

    Setting the project.clj URL & Description.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sqlinsql "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library for using SQL"
+  :url "http://github.com/krisajenkins/sqlinsql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
