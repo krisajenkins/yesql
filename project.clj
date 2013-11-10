b4e41d4f33609bc7b9051c242a2223a5b76ee35e
Kris Jenkins <krisajenkins@gmail.com>
2013-11-10 17:12:21 +0000

    Updating the JDBC library.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/java.jdbc "0.3.0-alpha5"]]
+                 [org.clojure/java.jdbc "0.3.0-beta1"]]
   :profiles {:dev {:dependencies [[org.apache.derby/derby "10.10.1.1"]]}}
   :scm {:name "git"
         :url "https://github.com/krisajenkins/yesql"})
