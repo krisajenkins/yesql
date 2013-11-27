b9809b36e0a6113144b71e5feaafca82b7e2bb88
Kris Jenkins <krisajenkins@gmail.com>
2013-11-27 12:29:26 +0000

    Refactoring the named-parameters test cases.
    Macros devour test-case boilerplate. Win.

--- test/yesql/named_parameters_test.clj
+++ test/yesql/named_parameters_test.clj
@@ -2,37 +2,49 @@
   (:require [clojure.test :refer :all]
             [yesql.named-parameters :refer :all]))
 
+(defmacro split-test
+  [category-name & args]
+  "For convenience, run a series of tests of the form <string => [split pieces]>.
+  The => symbol is mandatory sugar."
+  (assert (zero? (mod (count args) 3)) "split-test requires three forms per case.")
+  `(testing ~category-name
+     ~(cons 'do
+            (for [[string _ result] (partition 3 args)]
+              `(is (= (split-at-parameters ~string)
+                      (quote ~result)))))))
+
 (deftest split-at-parameters-test
-  (testing "Simple"
-    (is (= (split-at-parameters "SELECT 1 FROM dual")
-           '["SELECT 1 FROM dual"]))
-    (is (= (split-at-parameters "SELECT ? FROM dual")
-           '["SELECT " ? " FROM dual"]))
-    (is (= (split-at-parameters "SELECT :value FROM dual")
-           '["SELECT " value " FROM dual"]))
-    (is (= (split-at-parameters "SELECT 'test'\nFROM dual")
-           '["SELECT 'test'\nFROM dual"]))
-    (is (= (split-at-parameters "SELECT :value, :other_value FROM dual")
-           '["SELECT " value ", " other_value " FROM dual"])))
-  (testing "Whitespace"
-    (is (= (split-at-parameters "SELECT :age-5 FROM dual")
-           '["SELECT " age "-5 FROM dual"])))
-  (testing "Mixed parameters"
-    (is (= (split-at-parameters "SELECT :value, ? FROM dual")
-           '["SELECT " value ", " ? " FROM dual"])))
-  (testing "Escapes"
-    (is (= (split-at-parameters "SELECT :value, :other_value, ':not_a_value' FROM dual")
-           '["SELECT " value ", " other_value ", ':not_a_value' FROM dual"]))
-    (is (= (split-at-parameters "SELECT 'not \\' :a_value' FROM dual")
-           '["SELECT 'not \\' :a_value' FROM dual"])))
-  (testing "Casting"
-    (is (= (split-at-parameters "SELECT :value, :other_value, 5::text FROM dual")
-           '["SELECT " value ", " other_value ", 5::text FROM dual"])))
-  (testing "Complex"
-    (is (= (split-at-parameters "SELECT :a+2*:b+age::int FROM users WHERE username = ? AND :b > 0")
-           '["SELECT " a "+2*" b "+age::int FROM users WHERE username = " ? " AND " b " > 0"]))
-    (is (= (split-at-parameters "SELECT :value1 + ? + value2 + ? + :value1\nFROM SYSIBM.SYSDUMMY1")
-           '["SELECT " value1 " + " ? " + value2 + " ? " + " value1 "\nFROM SYSIBM.SYSDUMMY1"]))))
+  (split-test "Simple"
+              "SELECT 1 FROM dual"                    => ["SELECT 1 FROM dual"]
+              "SELECT ? FROM dual"                    => ["SELECT " ? " FROM dual"]
+
+              "SELECT :value FROM dual"               => ["SELECT " value " FROM dual"]
+              "SELECT 'test'\nFROM dual"              => ["SELECT 'test'\nFROM dual"]
+              "SELECT :value, :other_value FROM dual" => ["SELECT " value ", " other_value " FROM dual"])
+  
+  (split-test "Tokenization rules"
+              "SELECT :age-5 FROM dual" => ["SELECT " age "-5 FROM dual"])
+
+  (split-test "Mixing named & placeholder parameters"
+              "SELECT :value, ? FROM dual" => ["SELECT " value ", " ? " FROM dual"])
+
+  (split-test "Escapes"
+              "SELECT :value, :other_value, ':not_a_value' FROM dual"
+              => ["SELECT " value ", " other_value ", ':not_a_value' FROM dual"]
+
+              "SELECT 'not \\' :a_value' FROM dual"
+              => ["SELECT 'not \\' :a_value' FROM dual"])
+
+  (split-test "Casting"
+              "SELECT :value, :other_value, 5::text FROM dual"
+              => ["SELECT " value ", " other_value ", 5::text FROM dual"])
+
+  (split-test "Complex"
+              "SELECT :a+2*:b+age::int FROM users WHERE username = ? AND :b > 0"
+              => ["SELECT " a "+2*" b "+age::int FROM users WHERE username = " ? " AND " b " > 0"]
+
+              "SELECT :value1 + ? + value2 + ? + :value1\nFROM SYSIBM.SYSDUMMY1"
+              => ["SELECT " value1 " + " ? " + value2 + " ? " + " value1 "\nFROM SYSIBM.SYSDUMMY1"]))
 
 (deftest reassemble-query-test
   (is (= (reassemble-query (split-at-parameters "SELECT age FROM users WHERE country = :country") ["gb"])
