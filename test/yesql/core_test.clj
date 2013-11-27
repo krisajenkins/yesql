781942608c4dc5994399cd7b7ab30c6b791f5776
Kris Jenkins <krisajenkins@gmail.com>
2013-11-27 11:54:42 +0000

    Changing the core tests to not repeatly redef queries.

--- test/yesql/core_test.clj
+++ test/yesql/core_test.clj
@@ -8,6 +8,9 @@
                :subname (gensym "memory:")
                :create true})
 
+(defquery current-time-query "yesql/current_time.sql")
+(defquery named-parameters-query "yesql/named_parameters.sql")
+
 (deftest startup-test-db
   (sql/with-connection derby-db
     (sql/with-query-results rows ["SELECT CURRENT_TIMESTAMP FROM SYSIBM.SYSDUMMY1"]
@@ -39,23 +42,19 @@
              "SELECT CURRENT_TIMESTAMP AS time\nFROM SYSIBM.SYSDUMMY1")))))
 
 (deftest defquery-test
-  (defquery current-time-query "yesql/current_time.sql")
   (let [[{current-time :time}] (current-time-query derby-db)]
     (is (instance? java.util.Date current-time)))
 
-  (defquery named-parameters-query "yesql/named_parameters.sql")
   (let [[{current-time :time}] (named-parameters-query derby-db 1 2 3 4)]
     (is (instance? java.util.Date current-time))))
 
 (deftest defquery-metadata-test
-  (defquery current-time-query "yesql/current_time.sql")
   (let [metadata (meta (var current-time-query))]
     (is (= (:doc metadata)
            "Just selects the current time.\nNothing fancy."))
     (is (= (:arglists metadata)
            '([db]))))
 
-  (defquery named-parameters-query "yesql/named_parameters.sql")
   (let [metadata (meta (var named-parameters-query))]
     (is (= (:doc metadata)
            "Here's a query with some named and some anonymous parameters.\n(...and some repeats.)"))
@@ -64,7 +63,6 @@
 
 (deftest transaction-handling-test
   ;; Running a query in a transaction and using the result outside of it should work as expected.
-  (defquery current-time-query "yesql/current_time.sql")
   (let [[{time :time}] (sql/db-transaction [connection derby-db]
-                                         (current-time-query connection))]
+                                           (current-time-query connection))]
     (is (instance? java.util.Date time))))
