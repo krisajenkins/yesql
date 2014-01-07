2600e5c91959f7ade33a819d6e2d4879c1953076
Kris Jenkins <krisajenkins@gmail.com>
2014-01-07 10:13:40 +0000

    Version 0.3.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yesql "0.3.0-SNAPSHOT"
+(defproject yesql "0.3.0"
   :description "A Clojure library for using SQL"
   :url "https://github.com/krisajenkins/yesql"
   :license {:name "Eclipse Public License"
