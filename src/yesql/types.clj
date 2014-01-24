b5f867387002470f1dc880315f16f198811afde6
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-01-24 10:15:58 +0100

    added query assembly and name-based decision for 'query' vs. 'execute!'.
    If the query name ends in '!' the function body will call
    'clojure.java.jdbc/execute!'; 'clojure.java.jdbc/query' otherwise.
    
    Also, if the DB comfiguration parameter is omitted, the plain query
    vector will be generated instead of running the query against a DB.

--- src/yesql/types.clj
+++ src/yesql/types.clj
@@ -2,11 +2,13 @@
   (:require [clojure.java.jdbc :as jdbc]
             [yesql.named-parameters :refer [split-at-parameters reassemble-query]]
             [yesql.util :refer [distinct-except slurp-from-classpath]]))
-(defrecord Query
-    [name docstring querystring])
+
+;; ## Protocol
 
 (defprotocol Definable
   (emit-def [query]))
+
+;; ## Query Emitter
 
 (defn replace-question-mark-with-gensym
   [parameter]
@@ -14,20 +16,67 @@
     (gensym "P_")
     parameter))
 
-(extend-type Query
+(defn- split-query->args
+  "Use the split-up query string to create the different kinds of argument lists:
+
+   - `:query-args`: the symbols for the tail of the query vector,
+   - `:function-args`: the symbols for the query assembly and execution functions,
+   - `:display-args`: the symbols to be attached in the metadata.
+
+   The result will be a hash map with these fields."
+  [split-query]
+  (let [args (filterv symbol? split-query)
+        query-args (mapv replace-question-mark-with-gensym args)]
+    (hash-map
+      :query-args    query-args
+      :function-args (distinct query-args)
+      :display-args  (distinct-except args #{'?}))))
+
+(defn- emit-query-assemble-form
+  "Emit function to reassemble a query."
+  [id {:keys [function-args query-args]} split-query]
+  `(defn- ~id
+     [~@function-args]
+     (reassemble-query '~split-query [~@query-args])))
+
+(defn- fn-symbol
+  "Attach metadata (docstring/argument lists) to the given symbol."
+  [id docstring querystring display-args]
+  (->> {:arglists `(quote ([~@display-args] [~'db ~@display-args]))
+        :doc (format "%s%n%n(use the %d-argument version to generate the query vector)"
+                     (or docstring "")
+                     (count display-args))
+        ::source (str querystring)}
+       (with-meta id)))
+
+(defn- emit-fn-form
+  "Emit function with two arities: one to run a query against a database (first parameter is
+   DB configuration), and the other to create the plain query vector."
+  [id assemble-id docstring querystring {:keys [display-args function-args]} split-query db-fn]
+  `(def ~(fn-symbol id docstring querystring display-args)
+     (fn
+       ([~@function-args] (~assemble-id ~@function-args))
+       ([DB# ~@function-args]
+        (->> (~assemble-id ~@function-args)
+             (~db-fn DB#))))))
+
+(defn- emit-query-fn
+  "Emit function to run a query. If the query name ends in `!` it will call `clojure.java.jdbc/execute!`,
+   otherwise `clojure.java.jdbc/query` will be used. The function will be able to produce a plain query vector
+   if no database configuration is given as the first parameter."
+  [{:keys [name docstring querystring]}]
+  (let [split-query (split-at-parameters querystring)
+        args (split-query->args split-query)
+        id (symbol name)
+        assemble-id (gensym (str "assemble-" name))
+        db-fn (if (.endsWith ^String name "!")
+                `jdbc/execute!
+                `jdbc/query)]
+    `(do
+       ~(emit-query-assemble-form assemble-id args split-query)
+       ~(emit-fn-form id assemble-id docstring querystring args split-query db-fn))))
+
+(defrecord Query [name docstring querystring]
   Definable
-  (emit-def [{:keys [name docstring querystring]}]
-    (let [dbsym (gensym "DB_")
-          split-query (split-at-parameters querystring)
-          arglist (filterv symbol? split-query)
-          query-arglist (mapv replace-question-mark-with-gensym arglist)
-          function-arglist (into [dbsym] (distinct query-arglist))
-          arglist-without-question-marks (distinct-except arglist #{'?})
-          display-arglist `([~'db ~@arglist-without-question-marks])]
-      `(def ~(with-meta (symbol name)
-               {:arglists `(quote ~display-arglist)
-                :doc docstring})
-         (fn ~function-arglist
-           (jdbc/query ~dbsym
-                       (reassemble-query '~split-query
-                                         ~query-arglist)))))))
+  (emit-def [this]
+    (emit-query-fn this)))
