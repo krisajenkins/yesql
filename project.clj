5e34d3ea32bbdbc4878be2681b25cb131d8c25f5
Kris Jenkins <krisajenkins@gmail.com>
2014-02-02 16:37:54 +0000

    Adding a 'lein test-all' command to check against Clojure versions.
    This is a trick I've picked up from the clojure.java.jdbc project.clj.

--- project.clj
+++ project.clj
@@ -6,6 +6,10 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/java.jdbc "0.3.0"]
                  [org.clojure/algo.monads "0.1.4"]]
-  :profiles {:dev {:dependencies [[org.apache.derby/derby "10.10.1.1"]]}}
   :scm {:name "git"
-        :url "https://github.com/krisajenkins/yesql"})
+        :url "https://github.com/krisajenkins/yesql"}
+  :profiles {:dev {:dependencies [[org.apache.derby/derby "10.10.1.1"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.1"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+  :aliases {"test-all" ["with-profile" "+1.4:+1.5:+1.6" "test"]})
