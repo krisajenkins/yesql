2b9f3e28548482af7ba4eb41be6073d6861df3b2
Kris Jenkins <krisajenkins@gmail.com>
2014-02-17 09:17:23 +0000

    Updating the 1.6 build dependency to Clojure 1.6.0-beta1.

--- project.clj
+++ project.clj
@@ -13,5 +13,5 @@
                    :plugins [[lein-autoexpect "1.0"]] }
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-beta1"]]}}
   :aliases {"test-all" ["with-profile" "+1.4:+1.5:+1.6" "test"]})
