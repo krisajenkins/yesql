35bab7d78319da245baa46451ea67488e475cc2c
Kris Jenkins <krisajenkins@gmail.com>
2014-03-06 11:05:28 +0000

    WIP: Adding core.typed annotations.

--- project.clj
+++ project.clj
@@ -8,12 +8,16 @@
                  [instaparse "1.3.4"]]
   :scm {:name "git"
         :url "https://github.com/krisajenkins/yesql"}
-  :profiles {:dev {:dependencies [[org.apache.derby/derby "10.11.1.1"]
-                                  [expectations "2.0.9"]]
+  :profiles {:dev {:dependencies [[expectations "2.0.9"]
+                                  [org.apache.derby/derby "10.11.1.1"]
+                                  [org.clojure/core.typed "0.2.68"]]
                    :plugins [[lein-autoexpect "1.2.2"]
                              [lein-expectations  "0.0.7"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha1"]]}}
-  :aliases {"test-all" ["with-profile" "+1.5:+1.6:+1.7" "expectations"]
+  :core.typed {:check [yesql.util yesql.parser]}
+  :aliases {"test-all" ["do"
+                        ["typed" "check"]
+                        ["with-profile" "+1.5:+1.6:+1.7" "expectations"]]
             "test-ancient" ["expectations"]})
--- src/yesql/util.clj
+++ src/yesql/util.clj
@@ -1,8 +1,16 @@
 (ns yesql.util
+  (:refer-clojure :exclude [defrecord])
   (:require [clojure.java.io :as io]
-            [instaparse.core :as instaparse])
-  (:import (java.io FileNotFoundException StringWriter)))
+            [instaparse.core :as instaparse]
+            [clojure.string :as string]
+            [clojure.core.typed :refer [ann cf tc-ignore Seq Option All IFn U Nothing]]
+            [yesql.annotations])
+  (:import (java.io FileNotFoundException StringWriter)
+           (java.net URL)))
 
+(ann distinct-except
+  (All [x]
+       [(Option (Seq x)) (Option (Set x)) -> (Option (Seq x))]))
 (defn distinct-except
   "Same as distinct, but keeps duplicates from the exceptions set."
   [coll exceptions]
@@ -19,36 +27,50 @@
                  xs seen)))]
     (step coll #{})))
 
+(ann whitespace?
+  (IFn [nil -> false]
+      [String -> Boolean]))
 (defn whitespace?
   [string]
-  (boolean
-   (re-matches #"^\s*$" string)))
+  (if string
+    (boolean
+     (re-matches #"^\s*$" string))
+    false))
 
+(ann underscores-to-dashes
+  (IFn [nil -> nil]
+      [String -> String]))
 (defn underscores-to-dashes
   [string]
-  (clojure.string/replace string "_" "-"))
+  (when string
+    (string/replace string "_" "-")))
 
+(ann str-non-nil [String * -> String])
 (defn str-non-nil
   "Exactly like `clojure.core/str`, except it returns an empty string
    with no args (whereas `str` would return `nil`)."
   [& args]
   (apply str "" args))
 
-(defn slurp-from-classpath
-  "Slurps a file from the classpath."
-  [path]
-  (if-let [url (io/resource path)]
-    (slurp url)
-    (throw (FileNotFoundException. path))))
+(ann slurp-from-classpath
+  [String -> (U String Nothing)])
+(tc-ignore
+ (defn slurp-from-classpath
+   "Slurps a file from the classpath."
+   [path]
+   (if-let [url (io/resource path)]
+     (slurp url)
+     (throw (FileNotFoundException. path)))))
 
-(defn process-instaparse-result
-  [parsed]
-  (cond
-   (instaparse/failure? parsed) (let [failure (instaparse/get-failure parsed)]
-                                  (binding [*out* (StringWriter.)]
-                                    (instaparse.failure/pprint-failure failure)
-                                    (throw (ex-info (.toString *out*)
-                                                    failure))))
-   (< 1 (count parsed)) (throw (ex-info "Ambiguous parse - please report this as a bug at https://github.com/krisajenkins/yesql/issues"
-                                        {:variations (count parsed)}))
-   :else (first parsed)))
+(tc-ignore
+ (defn process-instaparse-result
+   [parsed]
+   (cond
+    (instaparse/failure? parsed) (let [failure (instaparse/get-failure parsed)]
+                                   (binding [*out* (StringWriter.)]
+                                     (instaparse.failure/pprint-failure failure)
+                                     (throw (ex-info (.toString *out*)
+                                                     failure))))
+    (< 1 (count parsed)) (throw (ex-info "Ambiguous parse - please report this as a bug at https://github.com/krisajenkins/yesql/issues"
+                                         {:variations (count parsed)}))
+    :else (first parsed))))
