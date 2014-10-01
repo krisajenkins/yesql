3c78bc974e7461af40b48102d5541083c5c196b7
Kris Jenkins <krisajenkins@gmail.com>
2014-10-01 08:52:40 +0100

    Tidying up some repetition in the generate-test file.

--- test/yesql/generate_test.clj
+++ test/yesql/generate_test.clj
@@ -1,31 +1,25 @@
 (ns yesql.generate-test
   (:require [expectations :refer :all]
+            [clojure.template :refer [do-template]]
             [yesql.generate :refer :all]))
 
-(expect #{}
-        (expected-parameter-list "SELECT * FROM user"))
+(do-template [statement _ expected-parameters]
+  (expect expected-parameters
+          (expected-parameter-list statement))
 
-(expect #{:?}
-        (expected-parameter-list "SELECT * FROM user WHERE user_id = ?"))
+  "SELECT * FROM user"
+  => #{}
 
-(expect #{:name}
-        (expected-parameter-list "SELECT * FROM user WHERE user_id = :name"))
+  "SELECT * FROM user WHERE user_id = ?"
+  => #{:?}
 
+  "SELECT * FROM user WHERE user_id = :name"
+  => #{:name}
 
-(expect #{:name :country :?}
-        (expected-parameter-list "SELECT * FROM user WHERE user_id = :name AND country = :country AND age IN (?,?)"))
+  "SELECT * FROM user WHERE user_id = :name AND country = :country AND age IN (?,?)"
+  => #{:name :country :?})
 
-;;; Testing reassemble-query
-(expect ["SELECT age FROM users WHERE country = ?" "gb"]
-        (rewrite-query-for-jdbc "SELECT age FROM users WHERE country = :country"
-                                {:country "gb"}))
-
-(expect ["SELECT age FROM users WHERE (country = ? OR country = ?) AND name = ?" "gb" "us" "tom"]
-        (rewrite-query-for-jdbc "SELECT age FROM users WHERE (country = ? OR country = ?) AND name = :name"
-                                {:? ["gb" "us"]
-                                 :name "tom"}))
-
-;;; Testing reassemble-query IN strings.
+;;; Testing in-list-parmaeter for "IN-list" statements.
 (expect [true true true false false]
         (map in-list-parameter?
              (list []
@@ -34,40 +28,55 @@
                    {:a 1}
                    #{1 2 3})))
 
+;;; Testing reassemble-query
+(do-template [statement parameters _ rewritten-form]
+  (expect rewritten-form
+          (rewrite-query-for-jdbc statement
+                                  parameters))
+
+  "SELECT age FROM users WHERE country = :country"
+  {:country "gb"}
+  => ["SELECT age FROM users WHERE country = ?" "gb"]
+
+  "SELECT age FROM users WHERE (country = ? OR country = ?) AND name = :name"
+  {:? ["gb" "us"]
+   :name "tom"}
+  => ["SELECT age FROM users WHERE (country = ? OR country = ?) AND name = ?" "gb" "us" "tom"]
+
 ;;; Vectors trigger IN expansion
-(expect ["SELECT age FROM users WHERE country = ? AND name IN (?,?,?)" "gb" "tom" "dick" "harry"]
-        (rewrite-query-for-jdbc "SELECT age FROM users WHERE country = :country AND name IN (:names)"
-                                {:country "gb"
-                                 :names ["tom" "dick" "harry"]}))
+  "SELECT age FROM users WHERE country = :country AND name IN (:names)"
+  {:country "gb"
+   :names ["tom" "dick" "harry"]}
+  => ["SELECT age FROM users WHERE country = ? AND name IN (?,?,?)" "gb" "tom" "dick" "harry"]
 
 ;;; Lists trigger IN expansion
-(expect ["SELECT age FROM users WHERE country = ? AND name IN (?,?,?)" "gb" "tom" "dick" "harry"]
-        (rewrite-query-for-jdbc "SELECT age FROM users WHERE country = :country AND name IN (:names)"
-                                {:country "gb"
-                                 :names (list "tom" "dick" "harry")}))
+  "SELECT age FROM users WHERE country = :country AND name IN (:names)"
+  {:country "gb"
+   :names (list "tom" "dick" "harry")}
+  => ["SELECT age FROM users WHERE country = ? AND name IN (?,?,?)" "gb" "tom" "dick" "harry"]
 
 ;;; Lazy seqs of cons of vectors trigger IN expansion
-(expect ["SELECT age FROM users WHERE country = ? AND name IN (?,?,?)" "gb" "tom" "dick" "harry"]
-        (rewrite-query-for-jdbc "SELECT age FROM users WHERE country = :country AND name IN (:names)"
-                                {:country "gb"
-                                 :names (lazy-seq (cons "tom" ["dick" "harry"]))}))
+  "SELECT age FROM users WHERE country = :country AND name IN (:names)"
+  {:country "gb"
+   :names (lazy-seq (cons "tom" ["dick" "harry"]))}
+  => ["SELECT age FROM users WHERE country = ? AND name IN (?,?,?)" "gb" "tom" "dick" "harry"]
 
 ;;; Maps do not trigger IN expansion
-(expect [ "INSERT INTO json (source, data) VALUES (?, ?)" "google" {:a 1}]
-        (rewrite-query-for-jdbc "INSERT INTO json (source, data) VALUES (:source, :data)"
-                                {:source "google"
-                                 :data {:a 1}}))
+  "INSERT INTO json (source, data) VALUES (:source, :data)"
+  {:source "google"
+   :data {:a 1}}
+  => ["INSERT INTO json (source, data) VALUES (?, ?)" "google" {:a 1}]
 
-(expect [ "INSERT INTO json (data, source) VALUES (?, ?)" {:a 1} "google"]
-        (rewrite-query-for-jdbc "INSERT INTO json (data, source) VALUES (:data, :source)"
-                                {:source "google"
-                                 :data {:a 1}}))
+  "INSERT INTO json (data, source) VALUES (:data, :source)"
+  {:source "google"
+   :data {:a 1}}
+  => ["INSERT INTO json (data, source) VALUES (?, ?)" {:a 1} "google"]
 
 ;;; Empty IN-lists are allowed by Yesql - though most DBs will complain.
-(expect ["SELECT age FROM users WHERE country = ? AND name IN ()" "gb"]
-        (rewrite-query-for-jdbc "SELECT age FROM users WHERE country = :country AND name IN (:names)"
-                                {:country "gb"
-                                 :names []}))
+  "SELECT age FROM users WHERE country = :country AND name IN (:names)"
+  {:country "gb"
+   :names []}
+  => ["SELECT age FROM users WHERE country = ? AND name IN ()" "gb"])
 
 ;;; Incorrect parameters.
 (expect AssertionError
