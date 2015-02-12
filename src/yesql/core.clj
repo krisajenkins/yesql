adfc925fec5c32a5fe5fcc6a2930451bf9d6c004
Ian Truslove <ian.truslove@sonian.net>
2015-02-12 05:07:12 +0000

    Added require-sql macro
    Conflicts:
    	src/yesql/core.clj

--- src/yesql/core.clj
+++ src/yesql/core.clj
@@ -9,12 +9,12 @@
   followed by optional comment lines (which form the docstring), followed by
   the query itself."
   ([filename]
-   (defqueries filename {}))
+     (defqueries filename {}))
   ([filename options]
-   (doall (->> filename
-               slurp-from-classpath
-               parse-tagged-queries
-               (map #(generate-var % options))))))
+     (doall (->> filename
+                 slurp-from-classpath
+                 parse-tagged-queries
+                 (map #(generate-var % options))))))
 
 (defn defquery*
   [name filename options]
@@ -33,6 +33,29 @@
   "Defines a query function, as defined in the given SQL file.
   Any comments in that file will form the docstring."
   ([name filename]
-   `(defquery ~name ~filename {}))
+     `(defquery ~name ~filename {}))
   ([name filename options]
-   `(defquery* ~(str name) ~filename ~options)))
+     `(defquery* ~(str name) ~filename ~options)))
+
+(defmacro require-sql
+  "Require-like behavior for yesql, to prevent namespace pollution.
+   Parameter is a list of [sql-source-file-name [:as alias] [:refer [var1 var2]]]
+   At least one of :as or :refer is required
+   Usage: (require-sql [\"sql/foo.sql\" :as foo-sql :refer [some-query-fn])"
+  [[sql-file & {:keys [as refer]} :as require-args]]
+  (when-not (or as refer)
+    (throw (Exception. "Missing an :as or a :refer")))
+  (let [current-ns (ns-name *ns*)
+        ;; Keep this .sql file's defqueries in a predictable place:
+        target-ns (symbol (str "yesquire/" sql-file))]
+    `(do
+       (ns-unalias *ns* '~as)
+       (create-ns '~target-ns)
+       (in-ns '~target-ns)
+       (clojure.core/require '[yesql.core])
+       (yesql.core/defqueries ~sql-file)
+       (clojure.core/in-ns '~current-ns)
+       ~(when as
+          `(clojure.core/alias '~as '~target-ns))
+       ~(when refer
+          `(clojure.core/refer '~target-ns :only '~refer)))))
