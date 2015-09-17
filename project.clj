cff75dba60077534e06d28f70e7fa8fe09679141
Sam Davies <seivadmas@gmail.com>
2015-09-17 14:33:40 -0300

    Add own project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yesql "0.5.0-rc4"
+(defproject org.clojars.seivadmas.yesql "0.5.0-rc4-sam-memoization-SNAPSHOT"
   :description "A Clojure library for using SQL"
   :url "https://github.com/krisajenkins/yesql"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,7 @@
                  [instaparse "1.4.1"]
                  [org.clojure/core.memoize "0.5.6"]]
   :scm {:name "git"
-        :url "https://github.com/krisajenkins/yesql"}
+        :url "https://github.com/samphilipd/yesql"}
   :profiles {:dev {:dependencies [[expectations "2.1.2"]
                                   [org.apache.derby/derby "10.11.1.1"]]
                    :plugins [[lein-autoexpect "1.4.0"]
