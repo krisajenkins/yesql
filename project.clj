8cbdc7c7ee3593544931b5bcb1dc68a21d0a103e
Kris Jenkins <krisajenkins@gmail.com>
2015-10-29 14:54:02 +0000

    Updating the tests for Clojure 1.8-beta2.

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-alpha4"]]}}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-beta2"]]}}
   :aliases {"test-all" ["with-profile" "+1.5:+1.6:+1.7:+1.8" "do"
                         ["clean"]
                         ["expectations"]]
