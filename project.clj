17225841ff8b8a13c24079939098febcd0e54112
Kris Jenkins <krisajenkins@gmail.com>
2016-04-27 14:46:19 +0100

    Adding pedantic version exclusions.

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
   :profiles {:dev {:dependencies [[expectations "2.1.3" :exclusions [org.clojure/clojure]]
                                   [org.apache.derby/derby "10.12.1.1"]]
                    :plugins [[lein-autoexpect "1.4.0"]
-                             [lein-expectations "0.0.8"]]}
+                             [lein-expectations "0.0.8" :exclusions [org.clojure/clojure]]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
