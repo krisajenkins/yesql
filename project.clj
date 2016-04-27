09e1a4cc6daf716bd9f89f5b7003304a3cc080ed
Kris Jenkins <krisajenkins@gmail.com>
2016-04-27 14:36:27 +0100

    Bumping the base Clojure version to 1.8.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/krisajenkins/yesql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/java.jdbc "0.4.2"]
                  [instaparse "1.4.1" :exclusions [org.clojure/clojure]]]
   :pedantic? :abort
