829537a1cf0ff83e5e09bbb06130bb85dd0970b9
Kris Jenkins <krisajenkins@gmail.com>
2017-09-09 12:38:18 +0100

    Updating the tests for Clojure 1.9.0-alpha20.

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha1"]]}}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha20"]]}}
   :aliases {"test-all" ["with-profile" "+1.5:+1.6:+1.7:+1.8:+1.9" "do"
                         ["clean"]
                         ["expectations"]]
