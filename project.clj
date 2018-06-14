bfca254838e44a45479e2454de8808652a2511a3
Ian Barrick <ian@servisuite.com>
2018-06-14 14:28:37 -0400

    bumped version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.batch/yesql "0.6.4"
+(defproject org.batch/yesql "0.6.5"
   :description "A Clojure library for using SQL"
   :url "https://github.com/krisajenkins/yesql"
   :license {:name "Eclipse Public License"
