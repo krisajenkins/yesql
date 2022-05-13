f37fe307cd444d561dc212aa69a8bfd29fc9e959
Mike Schaeffer <mike@mschaef.com>
2022-05-13 07:43:57 -0400

    Update version to indicate snapshot status.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yesql "0.5.3"
+(defproject yesql "0.5.4-SNAPSNOT"
   :description "A Clojure library for using SQL"
   :url "https://github.com/krisajenkins/yesql"
   :license {:name "Eclipse Public License"
@@ -9,7 +9,7 @@
   :pedantic? :abort
   :scm {:name "git"
         :url "https://github.com/krisajenkins/yesql"}
-  :profiles {:dev {:dependencies [[expectations "2.1.8" :exclusions [org.clojure/clojure]]
+  :profiles {:dev {:dependencies [[expectations "2.1.10" :exclusions [org.clojure/clojure]]
                                   [org.apache.derby/derby "10.15.2.0"]]
                    :plugins [[lein-autoexpect "1.4.0"]
                              [lein-expectations "0.0.8" :exclusions [org.clojure/clojure]]]}
